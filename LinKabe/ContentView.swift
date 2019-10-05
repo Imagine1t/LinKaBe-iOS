4017a27ae0f8fdfd4121e91b8330796fabd382d4
Crimson Yang <eva01.crimson@gmail.com>
2019-10-05 12:57:35 +0800

    Initial Commit

---
+++ LinKabe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinKabe
+//
+//  Created by Crimson Yang on 2019/10/5.
+//  Copyright © 2019 Crimson Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
